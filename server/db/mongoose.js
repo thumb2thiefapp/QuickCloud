c59199fdf19915596e01e9cd74e4366c696fbd07
thumb2thiefapp <34790405+thumb2thiefapp@users.noreply.github.com>
2018-01-11 17:49:15 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/QuickBackUp');
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 
 module.exports = {mongoose};
